87cd514b9b7e8e55211d6d03ae5286c108b65222
abrardfnadhif <abrar.d.f.nadhif@gdplabs.id>
2024-11-06 15:50:18 +0700

    Initial Commit

---
+++ EmojiApp/EmojiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiAppApp.swift
+//  EmojiApp
+//
+//  Created by Abrar Dwi Fairuz Nadhif on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct EmojiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
